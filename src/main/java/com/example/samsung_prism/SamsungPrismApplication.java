0d39c49fc28f1f6c7a3fed8fc37e2dc89eb3a4bc
lakhan_nad <lakhannad1999@gmail.com>
2021-03-30 19:40:49 +0530

    initial state

---
+++ src/main/java/com/example/samsung_prism/SamsungPrismApplication.java
@@ -0,0 +1,13 @@
+package com.example.samsung_prism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsungPrismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamsungPrismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samsung_prism/SamsungPrismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samsung_prism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsungPrismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
